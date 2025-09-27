b7e89a11f0f0150b9df3cfa89151e9d8e7aa9456
DEStonebreaker <donovanstonebreaker@gmail.com>
2025-09-27 10:43:11 -0400

    morning push

---
+++ Avalonia.TheraCare/Views/PatientWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia.TheraCare.Views;
+
+public partial class PatientWindow : UserControl
+{
+    public PatientWindow()
+    {
+        InitializeComponent();
+    }
+}
