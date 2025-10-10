ee112fd2cf5b03eea811f63cc4eac2696060077e
DEStonebreaker <donovanstonebreaker@gmail.com>
2025-10-10 00:54:50 -0400

    ...

--- Library.TheraCare/Models/Appointment.cs
+++ Library.TheraCare/Models/Appointment.cs
@@ -2,6 +2,10 @@ namespace Library.TheraCare.Models;
 
 public class Appointment
 {
+    /**
+     * Setup so that a physician holds a list of appointments
+     */
+    
     public DateTime? StartTime { get; init; } = null;
     public bool IsBooked { get; init; } = false;
 
